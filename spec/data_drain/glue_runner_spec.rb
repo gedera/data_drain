5a02b90b85debd58bf1c3f1a9f0887487dc2f2e1
Gabriel <gab.edera@gmail.com>
2026-04-13 21:32:08 -0300

    test: cobertura Storage::Local y GlueRunner

---
+++ spec/data_drain/glue_runner_spec.rb
@@ -0,0 +1,111 @@
+# frozen_string_literal: true
+
+RSpec.describe DataDrain::GlueRunner do
+  let(:config) do
+    DataDrain::Configuration.new.tap do |c|
+      c.aws_region = "us-east-1"
+      c.logger = Logger.new(StringIO.new)
+    end
+  end
+
+  before do
+    allow(DataDrain).to receive(:configuration).and_return(config)
+  end
+
+  describe ".run_and_wait" do
+    let(:mock_client) { instance_double("AwsGlueClient") }
+
+    before do
+      stub_const("Aws::Glue::Client", Class.new do
+        def initialize(**opts); end
+      end)
+      allow(Aws::Glue::Client).to receive(:new).and_return(mock_client)
+    end
+
+    it "retorna true cuando SUCCEEDED inmediato" do
+      start_response = double("start_resp", job_run_id: "run-123")
+      run_info = double("run_info", job_run_state: "SUCCEEDED", error_message: nil)
+
+      expect(mock_client).to receive(:start_job_run)
+        .with(hash_including(job_name: "my-job"))
+        .and_return(start_response)
+      expect(mock_client).to receive(:get_job_run)
+        .with(hash_including(job_name: "my-job", run_id: "run-123"))
+        .and_return(double("get_resp", job_run: run_info))
+
+      result = described_class.run_and_wait("my-job", { "--key" => "val" })
+      expect(result).to be true
+    end
+
+    it "hace polling hasta SUCCEEDED" do
+      start_response = double("start_resp", job_run_id: "run-123")
+      running_info = double("run_info", job_run_state: "RUNNING", error_message: nil)
+      succeeded_info = double("run_info", job_run_state: "SUCCEEDED", error_message: nil)
+
+      expect(mock_client).to receive(:start_job_run)
+        .and_return(start_response)
+      expect(mock_client).to receive(:get_job_run).and_return(double("get_resp", job_run: running_info))
+      expect(mock_client).to receive(:get_job_run).and_return(double("get_resp", job_run: succeeded_info))
+
+      allow(Kernel).to receive(:sleep)
+
+      result = described_class.run_and_wait("my-job", {}, polling_interval: 5)
+      expect(result).to be true
+    end
+
+    it "levanta RuntimeError cuando FAILED" do
+      start_response = double("start_resp", job_run_id: "run-456")
+      run_info = double("run_info", job_run_state: "FAILED", error_message: "Out of memory")
+
+      expect(mock_client).to receive(:start_job_run).and_return(start_response)
+      expect(mock_client).to receive(:get_job_run)
+        .and_return(double("get_resp", job_run: run_info))
+
+      expect do
+        described_class.run_and_wait("failing-job")
+      end.to raise_error(RuntimeError, /failing-job/)
+    end
+
+    it "levanta RuntimeError cuando STOPPED" do
+      start_response = double("start_resp", job_run_id: "run-789")
+      run_info = double("run_info", job_run_state: "STOPPED", error_message: nil)
+
+      expect(mock_client).to receive(:start_job_run).and_return(start_response)
+      expect(mock_client).to receive(:get_job_run)
+        .and_return(double("get_resp", job_run: run_info))
+
+      expect do
+        described_class.run_and_wait("stopped-job")
+      end.to raise_error(RuntimeError, /STOPPED/)
+    end
+
+    it "trunca error_message a 200 chars" do
+      start_response = double("start_resp", job_run_id: "run-999")
+      long_msg = "x" * 300
+      run_info = double("run_info", job_run_state: "FAILED", error_message: long_msg)
+
+      expect(mock_client).to receive(:start_job_run).and_return(start_response)
+      expect(mock_client).to receive(:get_job_run)
+        .and_return(double("get_resp", job_run: run_info))
+
+      expect do
+        described_class.run_and_wait("failing-job")
+      end.to raise_error(RuntimeError) do |e|
+        expect(e.message.length).to be <= 220
+      end
+    end
+
+    it "levanta cuando hay error_message" do
+      start_response = double("start_resp", job_run_id: "run-101")
+      run_info = double("run_info", job_run_state: "FAILED", error_message: "Out of memory")
+
+      expect(mock_client).to receive(:start_job_run).and_return(start_response)
+      expect(mock_client).to receive(:get_job_run)
+        .and_return(double("get_resp", job_run: run_info))
+
+      expect do
+        described_class.run_and_wait("failing-job")
+      end.to raise_error(RuntimeError, /FAILED/)
+    end
+  end
+end
